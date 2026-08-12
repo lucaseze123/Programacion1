a322b8417f842d793fc663a37247129359865eab
lucaseze123 <lucasbidartgauna@gmail.com>
2026-08-12 13:00:33 -0300

    Ejercicios clase 1

--- src/introduccion_a_Java/Clase_1.java
+++ src/introduccion_a_Java/Clase_1.java
@@ -2,8 +2,34 @@ package introduccion_a_Java;
 
 public class Clase_1 {
 
+	public static int cantDivisores(int num) { // Contamos la cantidad de divisores de un numero
+		int contDiv = 0;
+		for (int i = 1; i <= num; i++) {
+			if (num % i == 0) {
+				contDiv++;
+			}
+		}
+		return contDiv;
+	}
+
+	public static boolean esPrimo(int num) { // Funcion que evalua si un numero es o no primo
+		if (cantDivisores(num) == 2) { // utilizamos la funcio anteriormente creada
+			return true;
+		}
+		return false;
+	}
+
+	public static int siguientePrimo(int num) {
+		num = num + 1;
+		while (!esPrimo(num)) {
+			num = num + 1;
+		}
+		return num;
+
+	}
+
 	public static void main(String[] args) {
-		
+		System.out.println(siguientePrimo(17));
 	}
 
 }
